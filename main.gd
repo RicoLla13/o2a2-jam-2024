6e0138fc76b4c57f1dc14dd326288bf15ea7a76f
Liviu Arsenescu <liviuarsenescu@gmail.com>
2024-07-06 23:27:53 +0200

    add tired emote, and test emote interractions in timeline

--- main.gd
+++ main.gd
@@ -3,9 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Dialogic.timeline_ended.connect(_on_timeline_end)
 	Dialogic.start("game_timeline")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_timeline_end():
+	Dialogic.timeline_ended.disconnect(_on_timeline_end)
+	get_tree().quit()
