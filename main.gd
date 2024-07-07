7a5bc76a8d66f86c0ae8e5129282462a57a26f30
Liviu <liviuarsenescu@gmail.com>
2024-07-07 18:05:06 +0200

    smoothing in menu and test procedure

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Dialogic.timeline_ended.connect(_on_timeline_end)
-	Dialogic.start("game_timeline")
+	Dialogic.start("gametest_timeline")
 
 func _on_timeline_end():
 	Dialogic.timeline_ended.disconnect(_on_timeline_end)
